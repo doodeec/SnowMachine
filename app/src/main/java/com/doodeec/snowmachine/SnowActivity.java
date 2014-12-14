f3132d3c0d89ff7ce3b78ea4d3774f087a36141f
doodeec <doodeec@gmail.com>
2014-12-14 11:35:31 +0100

    versions

--- app/src/main/java/com/doodeec/snowmachine/SnowActivity.java
+++ app/src/main/java/com/doodeec/snowmachine/SnowActivity.java
@@ -13,6 +13,8 @@ import java.util.List;
 public class SnowActivity extends ActionBarActivity {
 
     private SnowWindow snowWindow;
+    private SnowWindow snowWindow2;
+    private SnowWindow snowWindow3;
     private DisplayMetrics displayMetrics = new DisplayMetrics();
 
     @Override
@@ -21,27 +23,37 @@ public class SnowActivity extends ActionBarActivity {
         setContentView(R.layout.activity_snow);
 
         snowWindow = (SnowWindow) findViewById(R.id.snow_window);
+        snowWindow2 = (SnowWindow) findViewById(R.id.snow_window_2);
+        snowWindow3 = (SnowWindow) findViewById(R.id.snow_window_3);
 
         int sizeX = getScreenWidth();
         int sizeY = getScreenHeight();
 
         List<SnowFlake> mFlakes = new ArrayList<>();
+        List<SnowFlake> mFlakes2 = new ArrayList<>();
+        List<SnowFlake> mFlakes3 = new ArrayList<>();
         for (int k = 0; k < 100; k++) {
             //big flakes
             mFlakes.add(new SnowFlake((float) Math.random() * sizeX, (float) Math.random() * sizeY, (float) (7 + Math.random() * 2)));
             //medium flakes
-            mFlakes.add(new SnowFlake((float) Math.random() * sizeX, (float) Math.random() * sizeY, (float) (3 + Math.random() * 2)));
+            mFlakes2.add(new SnowFlake((float) Math.random() * sizeX, (float) Math.random() * sizeY, (float) (3 + Math.random() * 2)));
             //tiny flakes
-            mFlakes.add(new SnowFlake((float) Math.random() * sizeX, (float) Math.random() * sizeY, (float) (1 + Math.random() * 2)));
+            mFlakes3.add(new SnowFlake((float) Math.random() * sizeX, (float) Math.random() * sizeY, (float) (1 + Math.random() * 2)));
         }
 
         snowWindow.setFlakes(mFlakes);
+        snowWindow2.setFlakes(mFlakes2);
+        snowWindow3.setFlakes(mFlakes3);
         snowWindow.setScreenSize(sizeX, sizeY);
+        snowWindow2.setScreenSize(sizeX, sizeY);
+        snowWindow3.setScreenSize(sizeX, sizeY);
     }
 
     @Override
     protected void onPause() {
         snowWindow.stopSnowing();
+        snowWindow2.stopSnowing();
+        snowWindow3.stopSnowing();
         super.onPause();
     }
 
@@ -49,6 +61,8 @@ public class SnowActivity extends ActionBarActivity {
     protected void onResume() {
         super.onResume();
         snowWindow.resumeSnowing();
+        snowWindow2.resumeSnowing();
+        snowWindow3.resumeSnowing();
     }
 
     private int getScreenWidth() {
